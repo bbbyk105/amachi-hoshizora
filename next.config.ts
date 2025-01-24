9c20d0c7ec17dc53ba8118f2161dfd201e45f135
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-01-24 08:17:18 +0100

    stripe_key

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  env: {
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  },
+};
+
 export default nextConfig;
